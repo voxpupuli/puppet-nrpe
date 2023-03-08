86ff95d6333a73498b86faa151dc5f0d8f50e66c
Tim Meusel <tim@bastelfreak.de>
2023-03-08 19:04:48 +0100

    rubocop: autofix

--- spec/functions/ssl_logging_spec.rb
+++ spec/functions/ssl_logging_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe 'nrpe::ssl_logging' do
@@ -6,6 +8,7 @@ describe 'nrpe::ssl_logging' do
       is_expected.to run.with_params(true, true, true, true, true, true).and_return('0x3f')
     end
   end
+
   context 'all logging options set to false' do
     it 'returns the string \'0x00\'' do
       is_expected.to run.with_params(false, false, false, false, false, false).and_return('0x00')
